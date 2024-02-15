ff4c0a1578a51da791bd356b60da5188eeb03164
Jessah Tumbaga <jessah.tumbaga@stu.ocsb.ca>
2024-02-15 19:52:24 +0000

    Added MathOperators.swift

---
+++ MathOperators.swift
@@ -0,0 +1,22 @@
+//
+//  MathOperators.swift
+//
+//  Created by Jessah
+//  Created on 2024-02-15
+//  Version 1.0
+//  Copyright (c) 2024 Jessah. All rights reserved.
+//
+//  Using math operators in swift.
+
+// Import for power.
+import Foundation
+
+// Print and perform math operations
+print("4 + 4 = \(4 + 4)")
+print("4 - 2 =  \(4 - 2)")
+print("4 * 3 = \(4 * 3)")
+print("20 / 4 = \(20 / 4)")
+print("4 / 3 = \(4.0 / 3.0)")
+print("4^2 = \(pow(4,2))")
+print("3^3 = \(pow(3,3))")
+print("√4 = \(4.squareRoot())")
